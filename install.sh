ac5c50dbd631d8e5de9c1efe0432d8cdd4d9f69c
Vinod Panicker <vinod.panicker@wipro.com>
2016-07-24 11:06:06 +0530

    added Install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+OAH_VERSION=0.0.1
+OAH_INSTALLER_TMP_FOLDER=/tmp/oah-installer
+OAH_SHELL_GITURL="https://github.com/openapphack/openapphack-shell.git"
+OAH_SHELL_REPO=openapphack-shell
+#CHECK GIT
+
+
+
+mkdir -p ${OAH_INSTALLER_TMP_FOLDER}
+cd /tmp/oah-installer
+
+
+git clone ${OAH_SHELL_GITURL}
+cd ${OAH_SHELL_REPO}
+./install.sh
+CHECK_OAH_INSTALL ${OAH_VERSION}
+rmdir ${OAH_INSTALLER_TMP_FOLDER}
+
+function CHECK_OAH_INSTALL
+{
+  version=$1
+  echo $(OAH ) | grep ${version}
+}
