cc301a2262b936b6be9f7a6f426e8c030dcb44de
Vinod Panicker <vinod.panicker@wipro.com>
2016-08-16 11:46:54 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
-OAH_VERSION=0.0.1
+OAH_VERSION=0.0.1a1
 OAH_INSTALLER_TMP_FOLDER=/tmp/oah-installer
-OAH_SHELL_GITURL="https://github.com/openapphack/openapphack-shell.git"
+OAH_SHELL_GITURL="https://github.com/openapphack/oah-shell.git"
 OAH_SHELL_REPO=openapphack-shell
 #CHECK GIT
 
+echo "Looking for git..."
+if [ -z $(which git) ]; then
+	echo "Not found."
+	echo "======================================================================================================"
+	echo " Please install git on your system using your favourite package manager."
+	echo ""
+	echo " Restart after installing git."
+	echo "======================================================================================================"
+	echo ""
+	exit 0
+fi
 
 
 mkdir -p ${OAH_INSTALLER_TMP_FOLDER}
-cd /tmp/oah-installer
+cd ${OAH_INSTALLER_TMP_FOLDER}
 
 
 git clone ${OAH_SHELL_GITURL}
